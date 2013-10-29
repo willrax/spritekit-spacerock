a8a5597c40e32bdf683e97d28774142f83454f64
Will Raxworthy <git@willrax.com>
2013-10-30 07:23:15 +1100

    Complete tutorial.

--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,5 +1,10 @@
 class AppDelegate
   def application(application, didFinishLaunchingWithOptions:launchOptions)
     true
+
+    scene_view = SceneViewController.alloc.init
+    @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
+    @window.rootViewController = scene_view
+    @window.makeKeyAndVisible
   end
 end
